73f16ddc36efc45fe3edf9e70755bf9589b5e86b
Vrashti Goel <vrashtigoel0503@gmail.com>
2024-11-29 15:49:02 +0530

    It is my first project on GIT.

---
+++ ASSIGNMENT-1/Operators.py
@@ -0,0 +1,12 @@
+# Calculate the sum, difference, product, and quotient of two numbers.
+# Perform various assignment operations on a variable.
+# Compare two numbers and print the results.
+# Check conditions using logical operators.
+# Check the identity of variables.
+# Use unary operators to change the sign of a number.
+# Perform bitwise operations on any two integers.
+# Use the ternary operator to assign values based on conditions.
+
+import math as M
+print(M.ceil(7.4))
+# print(M.int(7.5))
